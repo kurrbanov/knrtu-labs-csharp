f70f4a8c7b1c59a16c1b5f9823ebf5f7b2f70769
Radif <32257685+JustxNick@users.noreply.github.com>
2020-09-09 13:33:59 +0300

    bag fixed

--- Labs/Lab2/Program.cs
+++ Labs/Lab2/Program.cs
@@ -13,38 +13,39 @@ namespace Lab2
         static void Task1()
         {
             // 8 variant
-            static void Task1()
-            {
-                double dx = Convert.ToDouble(Console.ReadLine());
-            
-                double x = -9;
+            double dx = Convert.ToDouble(Console.ReadLine());
 
-                while (x <= 7)
+            double x = -9;
+
+            while (x <= 7)
+            {
+                if (x >= -9 && x <= -7)
                 {
-                    if (x >= -9 && x <= -7)
-                    {
-                        Console.Write("y " + Convert.ToString(x) + ": ");
-                        Console.WriteLine(0);
-                    } else if (x > -7 && x <= -3)
-                    {
-                        Console.Write("y " + Convert.ToString(x) + ": ");
-                        Console.WriteLine(x + 7);
-                    } else if (x >= -2 && x <= 2)
-                    {
-                        Console.Write("y " + Convert.ToString(x) + ": ");
-                        Console.WriteLine(x * x);
-                    } else if (x >= 2 && x <= 4)
-                    {
-                        Console.Write("y " + Convert.ToString(x) + ": ");
-                        Console.WriteLine(-2 * x + 8);
-                    } else if (x >= 4 && x <= 7)
-                    {
-                        Console.Write("y " + Convert.ToString(x) + ": ");
-                        Console.WriteLine(0);
-                    }
-                    x += dx;
+                    Console.Write("y " + Convert.ToString(x) + ": ");
+                    Console.WriteLine(0);
+                }
+                else if (x > -7 && x <= -3)
+                {
+                    Console.Write("y " + Convert.ToString(x) + ": ");
+                    Console.WriteLine(x + 7);
+                }
+                else if (x >= -2 && x <= 2)
+                {
+                    Console.Write("y " + Convert.ToString(x) + ": ");
+                    Console.WriteLine(x * x);
+                }
+                else if (x >= 2 && x <= 4)
+                {
+                    Console.Write("y " + Convert.ToString(x) + ": ");
+                    Console.WriteLine(-2 * x + 8);
+                }
+                else if (x >= 4 && x <= 7)
+                {
+                    Console.Write("y " + Convert.ToString(x) + ": ");
+                    Console.WriteLine(0);
                 }
 
+                x += dx;
             }
         }
 
@@ -63,7 +64,7 @@ namespace Lab2
                     SumOfNum2 += num2 % 10;
                     num2 /= 10;
                 }
-                
+
                 if (SumOfNum1 == SumOfNum2)
                 {
                     Console.Write(i);
@@ -71,7 +72,7 @@ namespace Lab2
                     Console.WriteLine(i * 2);
                 }
             }
-            
+
             // variant 2
             for (int i = 10; i <= 99; i++)
             {
@@ -82,7 +83,7 @@ namespace Lab2
                 else if (i >= 32 && num / 1000 == num % 10 && (num / 100) % 10 == (num % 100) / 10)
                     Console.WriteLine(i);
             }
-            
+
             // variant 3  DON'T touch them please.
             int N = Convert.ToInt32(Console.ReadLine());
 
@@ -94,26 +95,26 @@ namespace Lab2
                 if (check % Convert.ToInt64(Pow(10, lenNum)) == 0)
                     Console.WriteLine(i);
             }
-            
+
             // variant 4, 5 - very easy
-            
+
             // variant 6
             for (int i = 0; i < 10; i++)
-                for (int j = 0; j < 10; j++)
-                {
-                    int num = Convert.ToInt32("42" + Convert.ToString(i) + Convert.ToString(j) + "4");
-                    
-                    if (num % 72 == 0) Console.WriteLine(num);
-                }
+            for (int j = 0; j < 10; j++)
+            {
+                int num = Convert.ToInt32("42" + Convert.ToString(i) + Convert.ToString(j) + "4");
+
+                if (num % 72 == 0) Console.WriteLine(num);
+            }
 
             // variant 7, 9 - easy
-            
+
             // variant 8
             for (int i = 100; i <= 999; i++)
             {
                 if (i % 11 == 0 && (i % 10 == 3 || i / 100 == 3 || (i / 10) % 10 == 3)) Console.WriteLine(i);
             }
-            
+
             //variant 10
             for (int i = 100; i < 1000; i++)
             {
