0541567c3b2ec798d566518f0264252e03463a06
Tsubasa <52043350+Hanekawa-chan@users.noreply.github.com>
2020-09-23 15:40:03 +0300

    Update Program.cs
    и 5ю добавил, жду 6

--- Labs/Laba5/Program.cs
+++ Labs/Laba5/Program.cs
@@ -100,5 +100,105 @@ namespace laba5
             }
             Console.WriteLine();
         }
+        //2 variant
+        public static void Task1_2()
+        {
+            char[] inp = Console.ReadLine().ToCharArray();
+            char t;
+            int l = inp.Length;
+            for (int i = 0; i < l / 2; i++)
+            {
+                t = inp[i];
+                inp[i] = inp[l - i - 1];
+                inp[l - i - 1] = t;
+            }
+            string outp = new string(inp);
+            Console.WriteLine(outp);
+        }
+        public static void Task2_2()
+        {
+            string inp = Console.ReadLine();
+            string[] words = inp.Split(' ');
+            int max = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (ValidateString(words[i]))
+                {
+                    max = words[i].Length > max ? words[i].Length : max;
+                }
+                else
+                {
+                    max = (words[i].Length - 1) > max ? words[i].Length - 1: max;
+                }
+            }
+
+            int k = 0;
+            
+            foreach (string str in words)
+            {
+                words[k] = enc(str, max);
+                k += 1;
+            }
+            string output = words[0];
+            for (int i = 1; i < words.Length; i++)
+            {
+                output = string.Concat(output, " ", words[i]);
+            }
+
+            Console.WriteLine(output + " " + max);
+        }
+        static string enc(String a, int b){
+            int l = a.Length;
+            char[] j = a.ToCharArray();
+            for (int i = 0; i < l; i++) {
+                char g = a[i];
+                if (System.Char.IsLetter(g)) 
+                {
+                    if (System.Char.IsUpper(g)) 
+                    {
+                        if (g + b <= 90){
+                            int c = (int) g + b;
+                            char d = (char) c;
+                            j[i] = d;
+                        }
+                        else 
+                        {
+                            int c = (int) g + b;
+                            int v = c - 26;
+                            char d = (char) v;
+                            j[i] = d;
+                        }
+                    }
+                    else {
+                        if (g + b <= 122)
+                        {
+                            int c = (int) g + b;
+                            char d = (char) c;
+                            j[i] = d;
+                        }
+                        else
+                        {
+                            int c = (int) g + b;
+                            int v = c - 26;
+                            char d = (char) v;
+                            j[i] = d;
+                        }
+                    }
+                }
+            }
+            return new string(j);
+        }
+        public static bool ValidateString(string str) {
+            str = str.ToLower();
+            char[] charArray = str.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++) 
+            {
+                char ch = charArray[i];
+                if (!(ch >= 'a' && ch <= 'z')) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
